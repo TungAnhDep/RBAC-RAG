f3ba175461cd01d53fee8943b6604ecff6efb11e
TungAnhDep <tunganhnguyenphu@gmail.com>
2026-04-08 22:08:45 +0700

    Initial commit

--- rbac-frontend/rbac-frontend/next.config.ts
+++ rbac-frontend/rbac-frontend/next.config.ts
@@ -8,7 +8,7 @@ const nextConfig = {
   //     },
   //   ];
   // },
-  output: "export",
+
   images: { unoptimized: true },
   eslint: { ignoreDuringBuilds: true },
   typescript: { ignoreBuildErrors: true },
--- rbac-frontend/rbac-frontend/src/app/api/proxy/[[...path]]/route.ts
+++ rbac-frontend/rbac-frontend/src/app/api/proxy/[[...path]]/route.ts
@@ -1,4 +1,5 @@
 export const runtime = "edge";
+export const dynamic = "force-dynamic";
 import { getRequestContext } from "@cloudflare/next-on-pages";
 
 export async function POST(req: Request) {

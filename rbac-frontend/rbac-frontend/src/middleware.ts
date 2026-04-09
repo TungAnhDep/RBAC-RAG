4448bca6ba5ef5d5f46e2327300ba90c9a90c9ee
TungAnhDep <tunganhnguyenphu@gmail.com>
2026-04-09 11:48:59 +0700

    Initial commit

--- rbac-frontend/rbac-frontend/src/middleware.ts
+++ rbac-frontend/rbac-frontend/src/middleware.ts
@@ -44,5 +44,5 @@ export async function middleware(req: NextRequest) {
 }
 
 export const config = {
-  matcher: ["/", "/admin/:path*", "/login"],
+  matcher: ["/", "/admin/:path*", "/login", "/api/:path*"],
 };
